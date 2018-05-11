c28d398ce9ef64e10e5ec61961aa2a674fb96920
Mikita Zakharanka <mikita_zakharanka@epam.com>
2018-05-11 16:30:56 +0300

    Fixed review issues

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ import { Dirwatcher, Importer } from './modules';
 const dirwatcher = new Dirwatcher();
 const importer = new Importer();
 
-const handleImport = (paths) => {
-    importer.import(paths)
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
+const logger = (error, data) => {
+    if (error) {
+        console.error(error);
+    }
+
+    console.log(data);
+};
+
+const handleImport = async (paths) => {
+    try {
+        const data = await importer.import(paths);
+
+        logger(null, data);
+    } catch (e) {
+        logger(e);
+    }
+};
+
+const handleSyncImport = (paths) => {
+    importer.importSync(paths, logger);
 };
 
 dirwatcher.watch('./data', 1000);
--- modules/Importer.js
+++ modules/Importer.js
@@ -3,26 +3,33 @@ import csv from 'csvtojson';
 
 export default class Importer {
     import(paths) {
-        const imports = paths.map((path) => {
-            return new Promise((resolve, reject) => {
-                csv().fromFile(path)
-                    .on('end_parsed', jsonObj => resolve(jsonObj))
-                    .on('done', error => reject(error));
-            });
-        });
+        const imports = paths.map(path => this.processFile(path));
 
         return Promise.all(imports);
     }
 
-    importSync(paths) {
+    importSync(paths, callback) {
+        const files = [];
+        let processedFiles = 0;
+
         paths.forEach((path) => {
             csv().fromFile(path)
-                .on('end_parsed', jsonObj => console.log(jsonObj));
+                .on('end_parsed', (jsonObj) => {
+                    files.push(jsonObj);
+                    processedFiles++;
+
+                    if (processedFiles === paths.length) {
+                        callback(null, files);
+                    }
+                });
         });
     }
 
-    /**
-     * ToDo: implement this method
-     */
-    importAsync() {}
+    processFile(path) {
+        return new Promise((resolve, reject) => {
+            csv().fromFile(path)
+                .on('end_parsed', jsonObj => resolve(jsonObj))
+                .on('done', error => reject(error));
+        });
+    }
 }
